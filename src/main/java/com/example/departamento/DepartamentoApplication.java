87ecb38197b8f5af7c8c9fab2868d38e1a669d3b
flavioaugustto <flavioaugustosilv@gmail.com>
2026-03-24 10:36:57 -0300

    Pastas organizadas

---
+++ src/main/java/com/example/departamento/DepartamentoApplication.java
@@ -0,0 +1,13 @@
+package com.example.departamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartamentoApplication.class, args);
+	}
+
+}
